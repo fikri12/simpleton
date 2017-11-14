c50c58ddda6fb7133a0cf9495a7847c7b83c7fbe
fikri12 <fikriaditia.aditia9@gmail.com>
2017-11-14 15:40:40 +0700

    flow

--- application/modules/main/controllers/Welcome.php
+++ application/modules/main/controllers/Welcome.php
@@ -20,51 +20,58 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$data['judul_besar'] = 'Simpleton';
+		$data['judul_besar'] = 'Dashboard';
 		$data['judul_kecil'] = 'Version 1.0.0';
-		$data['output'] = '<div class="content body">
-							<section id="introduction">
-							  <h2 class="page-header">Intro</h2>
-							  <p class="lead">
-							    <b>Simpleton</b> dibuat untuk memudahkan kamu dalam membuat program PHP yang selalu membutuhkan Autentikasi dan operasi CRUD (Create, Read, Update & Delete).
-							    Daripada membuat autentikasi dan CRUD berulang-ulang, lebih baik memakai <b>Simpleton</b> Saja :D
-							    Skeleton Codeigniter ini menggunakan template <a href = "https://almsaeedstudio.com/preview">AdminLTE</a>, salah satu template gratis terpopuler saat ini. <b>Simpleton</b> cocok untuk kamu yang familiar dengan :<br><br>
-							    1. <a href = "http://www.codeigniter.com/download">Codeigniter</a><br>
-							    2. <a href = "http://benedmunds.com/ion_auth/">Ion Auth</a><br>
-							    3. <a href = "http//grocerycrud.com">GroceryCRUD</a><br><br>
-							    Punya saran atau ingin diskusi tentang <b>Simpleton</b> ? <a href = "https://www.facebook.com/groups/924598607617619">Disini tempatnya.</a><br>
-							    Suka dengan <b>Simpleton</b> ? Silahkan like fanpagenya : <a href = "https://www.facebook.com/heruprambadicom-1650504335226276">Disini tempatnya.</a><br>
-							  </p>
-							</section><!-- /#introduction -->
-							<!-- ============================================================= -->
+		$data['output'] = '            <!-- Right side column. Contains the navbar and content of the page -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Dashboard
+                        <small>Version 2.0</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                        <li class="active">Dashboard</li>
+                    </ol>
+                </section>
 
-							<section id="download">
-							  <div class="row">
-							    <div class="col-sm-6">
-							      <div class="box box-primary">
-							        <div class="box-header with-border">
-							          <h3 class="box-title">Punya saran atau ingin diskusi tentang <b>Simpleton</b> ?</h3>
-							          <span class="label label-primary pull-right"><i class="fa fa-html5"></i></span>
-							        </div><!-- /.box-header -->
-							        <div class="box-body">
-							          <p>Silahkan beri masukan, atau request fitur tambahan di group dibawah ini.</p>
-							          <a class="btn btn-primary" href="https://www.facebook.com/groups/924598607617619" target = "_blank">Group Simpleton</a>
-							        </div><!-- /.box-body -->
-							      </div><!-- /.box -->
-							    </div><!-- /.col -->
-							    <div class="col-sm-6">
-							      <div class="box box-danger">
-							        <div class="box-header with-border">
-							          <h3 class="box-title">Suka dengan <b>Simpleton</b> ?</h3>
-							          <span class="label label-danger pull-right"><i class="fa fa-database"></i></span>
-							        </div><!-- /.box-header -->
-							        <div class="box-body">
-							          <p>Silahkan like fanpagenya.</p>
-							          <a class="btn btn-danger" href = "https://www.facebook.com/heruprambadicom-1650504335226276" target = "_blank">Disini tempatnya.</a>
-							        </div><!-- /.box-body -->
-							      </div><!-- /.box -->
-							    </div><!-- /.col -->
-							  </div><!-- /.row -->';
+                <!-- Main content -->
+                <section class="content">
+                    <!-- Info boxes -->
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Usulan</span>
+                                    <span class="info-box-number">2<small>Usulan</small></span>
+                                </div><!-- /.info-box-content -->
+                            </div><!-- /.info-box -->
+                        </div><!-- /.col -->
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Pending</span>
+                                    <span class="info-box-number">41,410</span>
+                                </div><!-- /.info-box-content -->
+                            </div><!-- /.info-box -->
+                        </div><!-- /.col -->
+
+                        <!-- fix for small devices only -->
+                        <div class="clearfix visible-sm-block"></div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Approved</span>
+                                    <span class="info-box-number">760</span>
+                                </div><!-- /.info-box-content -->
+                            </div><!-- /.info-box -->
+                        </div><!-- /.col --> 
+                    </div><!-- /.row -->';
 		$this->load->view('welcome_message', $data);
 	}
 }
